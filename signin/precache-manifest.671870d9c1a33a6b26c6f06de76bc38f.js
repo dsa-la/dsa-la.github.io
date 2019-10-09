555425b2558afa97849725ca275fee1a76534788
Jeff Cuevas-Koch <jeff@cuevaskoch.com>
2019-10-09 12:59:20 -0700

    Add signin demo

---
+++ signin/precache-manifest.671870d9c1a33a6b26c6f06de76bc38f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b2e460a5bbcc23fbf1c3329a8f842a",
+    "url": "/signin/index.html"
+  },
+  {
+    "revision": "ff6b2cddf909894f96c0",
+    "url": "/signin/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e13dafe2a2baddef4796",
+    "url": "/signin/static/css/main.ed459d38.chunk.css"
+  },
+  {
+    "revision": "ff6b2cddf909894f96c0",
+    "url": "/signin/static/js/2.124cf3e9.chunk.js"
+  },
+  {
+    "revision": "e13dafe2a2baddef4796",
+    "url": "/signin/static/js/main.0731d68e.chunk.js"
+  },
+  {
+    "revision": "73d63a85dd327d2b77f5",
+    "url": "/signin/static/js/runtime-main.1ca09f26.js"
+  },
+  {
+    "revision": "6020d4a503f6e9eb3002018545443a7e",
+    "url": "/signin/static/media/loading.6020d4a5.png"
+  },
+  {
+    "revision": "ce37af657d56f2bb39eb67e3980011b5",
+    "url": "/signin/static/media/logo.ce37af65.png"
+  }
+]);
